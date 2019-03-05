55ac7663b485c1655c713e8f36390a55703f8274
Timur Garipov <tbgaripov@gmail.com>
2019-03-06 00:34:27 +0300

    Initial Commit

---
+++ Set_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Set_1
+//
+//  Created by Тимур Гарипов on 06/03/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
